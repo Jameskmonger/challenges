0d4c0fb44358eeacb81d9c64da7e7a1004870b96
James Monger <jameskmonger@hotmail.co.uk>
2016-01-05 23:17:39 +0000

    Recognise invalid inputs

--- problems/yoda-speak/solution.ts
+++ problems/yoda-speak/solution.ts
@@ -1,7 +1,28 @@
 import {ISolution} from '../solution.interface';
 
 export class Solution implements ISolution<string, string> {
+  static PRONOUNS: string[] = [
+    "I", "YOU", "HE", "SHE", "IT", "WE", "THEY"
+  ];
+
+  static INVALID: string = "Too difficult, this sentence is.";
+
   solve(input: string) {
+    var parts = input.split(' ');
+    if (!Solution.EqualsAnyPronoun(parts[0])) {
+      return Solution.INVALID;
+    }
+
     return "yoda text output";
+  }
+
+  static EqualsAnyPronoun(input: string): boolean {
+    for (var p of Solution.PRONOUNS) {
+      if (p === input.toUpperCase()) {
+        return true;
+      }
+    }
+
+    return false;
   }
 }
