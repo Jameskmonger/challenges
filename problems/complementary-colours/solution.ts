3b436f8aa0c4476b53f8359f6f15134afc5adec2
James Monger <jameskmonger@hotmail.co.uk>
2016-01-05 23:56:26 +0000

    Almost complete solution

--- problems/complementary-colours/solution.ts
+++ problems/complementary-colours/solution.ts
@@ -1,7 +1,32 @@
 import {ISolution} from '../solution.interface';
 
 export class Solution implements ISolution<string, string> {
+  static PAIRS = {
+    '0': 'f',
+    '1': 'e',
+    '2': 'd',
+    '3': 'c',
+    '4': 'b',
+    '5': 'a',
+    '6': '9',
+    '7': '8',
+    '8': '7',
+    '9': '6',
+    'a': '5',
+    'b': '4',
+    'c': '3',
+    'd': '2',
+    'e': '1',
+    'f': '0'
+  };
+
   solve(input: string) {
-    return "invalid";
+    var output = "#";
+
+    for (var i = 1; i < 6; i++) {
+      output += Solution.PAIRS[input[i]];
+    }
+
+    return output;
   }
 }
