e8f0f62abf1329b9469441348f68132f0c18230c
James Monger <jameskmonger@hotmail.co.uk>
2016-01-05 23:13:04 +0000

    Test suite complete

--- gulpfile.ts
+++ gulpfile.ts
@@ -18,17 +18,54 @@ gulp.task('challenge', function() {
   {
     var Solution = require(solutionPath).Solution;
   } catch (err) {
-    console.log("ERROR: Unable to find challenge '" + challenge + "'");
+    console.log("ERROR: Unable to find solution file for '" + challenge + "'");
     return;
   }
 
   var solution = new Solution();
 
-  console.log(solution.solve("bla"));
+  var testsPath: string = Utilities.GetTestsPath(challenge);
+
+  try
+  {
+    var Tests = require(testsPath).Tests;
+  } catch (err) {
+    console.log("ERROR: Unable to find tests file for '" + challenge + "'");
+    return;
+  }
+
+  if (Tests.tests === undefined) {
+    console.log("ERROR: Tests file must contain an array called 'tests'");
+  }
+
+  var totalTests = 0;
+  var passedTests = 0;
+  for (var test of Tests.tests) {
+    totalTests++;
+
+    var output = solution.solve(test.input);
+
+    if (output === test.output) {
+      passedTests++;
+      console.log("PASS: Test passed for input '" + test.input + "'");
+    } else {
+      console.log("FAIL: Test failed for input '" + test.input + "'");
+      console.log("    - Expected: " + test.output);
+      console.log("    - Actual: " + output);
+    }
+  }
+
+  console.log("FINISHED: Total tests: " + totalTests);
+  console.log("FINISHED: Passed tests: " + passedTests);
+  console.log("FINISHED: Failed tests: " + (totalTests - passedTests));
 });
 
 class Utilities {
   static GetSolutionPath(challengeName: string) {
     return ('./problems/' + challengeName + '/solution');
+  }
+
+  static GetTestsPath(challengeName: string) {
+    return ('./problems/' + challengeName + '/tests');
   }
 }
