7dc8c6223ead671d29151b132c5033da0dfbff80
James Monger <jameskmonger@hotmail.co.uk>
2016-01-06 00:03:16 +0000

    Generate pairs programmatically

--- problems/complementary-colours/solution.ts
+++ problems/complementary-colours/solution.ts
@@ -1,32 +1,16 @@
 import {ISolution} from '../solution.interface';
 
 export class Solution implements ISolution<string, string> {
-  static PAIRS = {
-    '0': 'f',
-    '1': 'e',
-    '2': 'd',
-    '3': 'c',
-    '4': 'b',
-    '5': 'a',
-    '6': '9',
-    '7': '8',
-    '8': '7',
-    '9': '6',
-    'a': '5',
-    'b': '4',
-    'c': '3',
-    'd': '2',
-    'e': '1',
-    'f': '0'
-  };
+  static CHARS = "0123456789abcdef";
 
   solve(input: string) {
-    var output = "#";
-
-    for (var i = 1; i < 7; i++) {
-      output += Solution.PAIRS[input[i]];
+    var pairs = {};
+    for (var c = 0; c < Solution.CHARS.length; c++) {
+      pairs[Solution.CHARS[c]] = Solution.CHARS[Solution.CHARS.length - 1 - c];
     }
 
-    return output;
+    return "#" + pairs[input[1]] + pairs[input[2]]
+                + pairs[input[3]] + pairs[input[4]]
+                + pairs[input[5]] + pairs[input[6]];
   }
 }
