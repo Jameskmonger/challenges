4419f94a3726d5aa4c1b7cffd4cafb16e2515f5a
James Monger <jameskmonger@hotmail.co.uk>
2016-01-08 13:15:54 +0000

    Add isSafe method

--- problems/superstitious-elevators/solution.ts
+++ problems/superstitious-elevators/solution.ts
@@ -4,4 +4,8 @@ export class Solution implements ISolution<number, string> {
   solve(input: number): string {
     return "bla";
   }
+
+  private isSafe(number: number): boolean {
+    return !(number === 13 || number.toString().indexOf("4") !== -1);
+  }
 }
