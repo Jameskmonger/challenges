c9eaf70f6e27e52bed35485dce8e1ef1b41d7c68
James Monger <jameskmonger@hotmail.co.uk>
2016-01-08 13:19:13 +0000

    Count the floors properly

--- problems/superstitious-elevators/solution.ts
+++ problems/superstitious-elevators/solution.ts
@@ -2,16 +2,24 @@ import {ISolution} from '../solution.interface';
 
 export class Solution implements ISolution<number, string> {
   solve(input: number): string {
-    let floors = [];
-    for (let f = -1; f < input; f++) {
-      if (this.isSafe(f)) {
-        floors.push(f);
+    let floors = [], pointer = -1;
+    while (floors.length != input) {
+      if (this.isSafe(pointer)) {
+        floors.push(pointer);
       }
+      pointer++;
     }
-    return "bla";
+
+    let out = "", counter = 0;
+    for (let f of floors) {
+      counter++;
+
+      out = out + f;
+    }
+    return out;
   }
 
   private isSafe(number: number): boolean {
-    return !(number === 13 || number.toString().indexOf("4") !== -1);
+    return !(number === 0 || number === 13 || number.toString().indexOf("4") !== -1);
   }
 }
