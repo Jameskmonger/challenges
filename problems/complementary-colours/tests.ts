9121d7ec97f489f8aca490421424ad4f5b53af52
James Monger <jameskmonger@hotmail.co.uk>
2016-01-08 13:06:37 +0000

    Add more robust test suite architecture for complementary-colours

--- problems/complementary-colours/tests.ts
+++ problems/complementary-colours/tests.ts
@@ -1,28 +1,33 @@
-export class Tests {
-  public static tests: any[] = [
-    {
-      input: "#ffffff",
-      output: "#000000"
-    },
-    {
-      input: "#abcdef",
-      output: "#543210"
-    },
-    {
-      input: "#badcab",
-      output: "#452354"
-    },
-    {
-      input: "#133742",
-      output: "#ecc8bd"
-    },
-    {
-      input: "#a1b2c3",
-      output: "#5e4d3c"
-    },
-    {
-      input: "#7f7f80",
-      output: "#80807f"
-    }
-  ];
+import {ITestSuite} from '../testsuite.interface';
+import {ITest} from '../test.interface';
+
+export class Tests implements ITestSuite<string, string> {
+  public getTests(): Array<ITest<string, string>> {
+      return [
+        {
+          input: "#ffffff",
+          output: "#000000"
+        },
+        {
+          input: "#abcdef",
+          output: "#543210"
+        },
+        {
+          input: "#badcab",
+          output: "#452354"
+        },
+        {
+          input: "#133742",
+          output: "#ecc8bd"
+        },
+        {
+          input: "#a1b2c3",
+          output: "#5e4d3c"
+        },
+        {
+          input: "#7f7f80",
+          output: "#80807f"
+        }
+      ];
+  }
 }
