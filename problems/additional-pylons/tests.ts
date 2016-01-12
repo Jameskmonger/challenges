9168101f640744c5b7dce6e9f60e8e0a034a4c47
James Monger <jameskmonger@hotmail.co.uk>
2016-01-12 13:48:51 +0000

    Add tests for additional-pylons

--- problems/additional-pylons/tests.ts
+++ problems/additional-pylons/tests.ts
@@ -3,6 +3,19 @@ import {ITest} from '../test.interface';
 
 export class Tests implements ITestSuite<string, string> {
   public getTests(): Array<ITest<string, string>> {
-    return [];
+    return [
+      {
+        input: "2 Probe Probe Probe Probe Stalker Zealot Carrier Probe Zealot",
+        output: "You must construct 1 additional pylon!"
+      },
+      {
+        input: "5 Mothership Carrier Probe Tempest HighTemplar",
+        output: "true"
+      },
+      {
+        input: "0 Mothership Colossus Zealot",
+        output: "You must construct 2 additional pylons!"
+      }
+    ];
   }
 }
