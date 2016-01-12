894a12fbe754b0ec193786992cef86c5853a5cf0
James Monger <jameskmonger@hotmail.co.uk>
2016-01-12 13:17:32 +0000

    Add tests for abacaba

--- problems/abacaba/tests.ts
+++ problems/abacaba/tests.ts
@@ -3,6 +3,40 @@ import {ITest} from '../test.interface';
 
 export class Tests implements ITestSuite<number, string> {
   public getTests(): Array<ITest<number, string>> {
-    return [];
+    return [
+      {
+        input: 0,
+        output: "a"
+      },
+      {
+        input: 1,
+        output: "aba"
+      },
+      {
+        input: 2,
+        output: "abacaba"
+      },
+      {
+        input: 3,
+        output: "abacabadabacaba"
+      },
+      {
+        input: 4,
+        output: "abacabadabacabaeabacabadabacaba"
+      },
+      {
+        input: 5,
+        output: "abacabadabacabaeabacabadabacabafabacabadabacabaeabacabadabacaba"
+      },
+      {
+        input: 6,
+        output: "abacabadabacabaeabacabadabacabafabacabadabacabaeabacabadabacabagabacabadabacabaeabacabadabacabafabacabadabacabaeabacabadabacaba"
+      },
+      {
+        input: 7,
+        output: "abacabadabacabaeabacabadabacabafabacabadabacabaeabacabadabacabagabacabadabacabaeabacabadabacabafabacabadabacabaeabacabadabacabaha" +
+                "bacabadabacabaeabacabadabacabafabacabadabacabaeabacabadabacabagabacabadabacabaeabacabadabacabafabacabadabacabaeabacabadabacaba"
+      }
+    ];
   }
 }
