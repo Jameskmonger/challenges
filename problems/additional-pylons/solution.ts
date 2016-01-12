f97d32c7f0587fe57f6cbdfefb4d0b4eab8ed6a9
James Monger <jameskmonger@hotmail.co.uk>
2016-01-12 13:45:53 +0000

    Added units to additional-pylons

--- problems/additional-pylons/solution.ts
+++ problems/additional-pylons/solution.ts
@@ -1,6 +1,27 @@
 import {ISolution} from '../solution.interface';
 
 export class Solution implements ISolution<string, string> {
+  units = {
+    "Probe": 1,
+    "Zealot": 2,
+    "Sentry": 2,
+    "Stalker": 2,
+    "HighTemplar": 2,
+    "DarkTemplar": 2,
+    "Immortal": 4,
+    "Colossus": 6,
+    "Archon": 4,
+    "Observer": 1,
+    "WarpPrism": 2,
+    "Phoenix": 2,
+    "MothershipCore": 2,
+    "VoidRay": 4,
+    "Oracle": 3,
+    "Tempest": 4,
+    "Carrier": 6,
+    "Mothership": 8
+  };
+
   solve(input: string): string {
     return null;
   }
